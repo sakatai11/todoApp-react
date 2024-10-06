795c5779932025e098edfc827989a5de37c7102b
taichisakai <sakai111893@gmail.com>
2024-10-06 19:20:54 +0900

    editModalコンポーネントの実装完了

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,10 @@
 import { TodoListProps } from "../types/todos";
-import { statusesPull } from "../status/statuses";
 import { useState } from "react";
-import Modal from "@mui/material/Modal";
 import DeleteModal from "./modal/DeleteModal";
-import StatusPullList from "./statusBox/StatusPullList";
-import { Box, TextField, Button, Typography } from "@mui/material";
-import { jstFormattedDate } from "../utils/dateUtils";
+import { Box, Button } from "@mui/material";
 import { linkify } from "../utils/textUtils";
 import ToggleButton from "@mui/material/ToggleButton";
 import PushPinIcon from "@mui/icons-material/PushPin";
-import CloseIcon from "@mui/icons-material/Close";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import EditModal from "./modal/EditModal";
 
@@ -41,13 +36,6 @@ const TodoList = ({
 }: TodoProps) => {
 	const { deleteTodo, editTodo, saveTodo, setEditId } = clickOption;
 	const [modalIsOpen, setModalIsOpen] = useState(false);
-
-	const handleClose = () => {
-		setModalIsOpen(false);
-		setError(false); //エラーのリセット
-		setEditId(null);
-		setInput({ text: "", status: "" }); // リセットする
-	};
 
 	// URLを検出してリンクに変換する関数
 	const displayText = (text: string) => {
@@ -177,106 +165,17 @@ const TodoList = ({
 				</Button>
 				{isEditing && (
 					// モーダル
-					<Modal
-						open={modalIsOpen}
-						onClose={handleClose}
-						aria-labelledby="modal-modal-text"
-					>
-						<Box
-							sx={{
-								position: "absolute",
-								display: "flex",
-								flexDirection: "column",
-								justifyContent: "center",
-								alignItems: "center",
-								width: "100%",
-								height: "100%",
-								top: "50%",
-								left: "50%",
-								transform: "translate(-50%, -50%)",
-							}}
-						>
-							<Box
-								sx={{
-									bgcolor: "#FFF",
-									maxWidth: 650,
-									width: "100%",
-									boxShadow: 24,
-									boxSizing: "border-box",
-									px: 4,
-									pt: 2.5,
-									pb: 4,
-									position: "relative",
-								}}
-							>
-								<Typography
-									component="span"
-									color="#9e9e9e"
-									fontSize="12px"
-									paddingBottom="8px"
-									display="block"
-								>
-									編集日時：{jstFormattedDate(todo.time)}
-								</Typography>
-								<TextField
-									id="modal-modal-text"
-									variant="outlined"
-									type="text"
-									fullWidth
-									value={input.text}
-									error={input.text ? undefined : error}
-									helperText={
-										!input.text && error ? "内容を入力してください" : null
-									}
-									multiline
-									rows={9}
-									onChange={(e) => setInput({ ...input, text: e.target.value })}
-								/>
-								<StatusPullList
-									// statusプルダウン
-									pullDownList={statusesPull}
-									input={{ ...input, status: input.status }} // input.statusを渡す
-									error={error}
-									setInput={(statusInput) =>
-										setInput({ ...input, status: statusInput.status })
-									}
-								/>
-								<CloseIcon
-									// 閉じる
-
-									sx={{
-										position: "absolute",
-										top: "-27px",
-										right: 0,
-										color: "#FFF",
-										cursor: "pointer",
-									}}
-									onClick={handleClose}
-								/>
-								<Box
-									sx={{
-										width: "100%",
-										display: "flex",
-										justifyContent: "center",
-										marginTop: 3,
-									}}
-								>
-									<Button
-										variant="contained"
-										sx={{ display: "block" }}
-										onClick={() => {
-											saveTodo();
-											if (input.text && input.status) {
-												setModalIsOpen(false);
-											}
-										}}
-									>
-										保存
-									</Button>
-								</Box>
-							</Box>
-						</Box>
-					</Modal>
+					<EditModal
+						todo={todo}
+						input={input}
+						error={error} 
+						modalIsOpen={modalIsOpen}
+						setError={setError}
+						setEditId={setEditId}
+						setInput={setInput}
+						setModalIsOpen={setModalIsOpen}
+						saveTodo={saveTodo}
+					/>
 				)}
 				<DeleteModal
 					onDelete={() => {
--- src/components/modal/EditModal.tsx
+++ src/components/modal/EditModal.tsx
@@ -1,28 +1,24 @@
-import { useState } from "react";
 import { Button, Box, Typography, TextField } from "@mui/material";
 import { TodoListProps } from "../../types/todos";
 import { jstFormattedDate } from "../../utils/dateUtils";
 import { statusesPull } from "../../status/statuses";
 import Modal from "@mui/material/Modal";
 import CloseIcon from "@mui/icons-material/Close";
-import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import StatusPullList from "../statusBox/StatusPullList";
 
 type EditProp = {
   todo: TodoListProps;
   input: { text: string; status: string }; // inputをオブジェクト型に変更
-  isEditing: boolean;
 	error: boolean;
+  modalIsOpen: boolean;
   setError: (error: boolean) => void;
   setEditId: (id: string | null) => void;
   setInput: (input: { text: string; status: string }) => void;
-  editTodo: (id: string) => void;
+  setModalIsOpen: (modalIsOpen: boolean) => void; 
   saveTodo: () => void;
-	// onEdit: () => void;
 };
 
-const EditModal = ({ todo, input, isEditing, error,setError, setEditId, setInput, editTodo, saveTodo}: EditProp) => {
-	const [modalIsOpen, setModalIsOpen] = useState(false);
+const EditModal = ({ todo, input, error, modalIsOpen, setError, setEditId, setInput, setModalIsOpen, saveTodo }: EditProp) => {
 
   const handleClose = () => {
 		setModalIsOpen(false);
@@ -32,139 +28,106 @@ const EditModal = ({ todo, input, isEditing, error,setError, setEditId, setInput
 	};
 
 	return (
-    <>
-      <Button
-        // variant="outlined"
+    <Modal
+      open={modalIsOpen}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-text"
+    >
+      <Box
         sx={{
-          // p:0,
-          minWidth: "auto",
-          "@media (max-width: 767px)": {
-            padding: 0.5,
-          },
-        }}
-        onClick={() => {
-          if (todo.id) {
-            setModalIsOpen(true);
-            editTodo(todo.id);
-          }
+          position: "absolute",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
         }}
       >
-        <ModeEditIcon
+        <Box
           sx={{
-            width: 20,
-            height: 20,
-            "@media (max-width: 767px)": {
-              width: 15,
-              height: 15,
-            },
+            bgcolor: "#FFF",
+            maxWidth: 650,
+            width: "100%",
+            boxShadow: 24,
+            boxSizing: "border-box",
+            px: 4,
+            pt: 2.5,
+            pb: 4,
+            position: "relative",
           }}
+        >
+          <Typography
+            component="span"
+            color="#9e9e9e"
+            fontSize="12px"
+            paddingBottom="8px"
+            display="block"
+          >
+            編集日時：{jstFormattedDate(todo.time)}
+          </Typography>
+          <TextField
+            id="modal-modal-text"
+            variant="outlined"
+            type="text"
+            fullWidth
+            value={input.text}
+            error={input.text ? undefined : error}
+            helperText={
+              !input.text && error ? "内容を入力してください" : null
+            }
+            multiline
+            rows={9}
+            onChange={(e) => setInput({ ...input, text: e.target.value })}
+          />
+          <StatusPullList
+            // statusプルダウン
+            pullDownList={statusesPull}
+            input={{ ...input, status: input.status }} // input.statusを渡す
+            error={error}
+            setInput={(statusInput) =>
+              setInput({ ...input, status: statusInput.status })
+            }
+          />
+          <CloseIcon
+            // 閉じる
 
-          {...isEditing && (
-            // モーダル
-            <Modal
-              open={modalIsOpen}
-              onClose={handleClose}
-              aria-labelledby="modal-modal-text"
+            sx={{
+              position: "absolute",
+              top: "-27px",
+              right: 0,
+              color: "#FFF",
+              cursor: "pointer",
+            }}
+            onClick={handleClose}
+          />
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              marginTop: 3,
+            }}
+          >
+            <Button
+              variant="contained"
+              sx={{ display: "block" }}
+              onClick={() => {
+                saveTodo();
+                if (input.text && input.status) {
+                  setModalIsOpen(false);
+                }
+              }}
             >
-              <Box
-                sx={{
-                  position: "absolute",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  width: "100%",
-                  height: "100%",
-                  top: "50%",
-                  left: "50%",
-                  transform: "translate(-50%, -50%)",
-                }}
-              >
-                <Box
-                  sx={{
-                    bgcolor: "#FFF",
-                    maxWidth: 650,
-                    width: "100%",
-                    boxShadow: 24,
-                    boxSizing: "border-box",
-                    px: 4,
-                    pt: 2.5,
-                    pb: 4,
-                    position: "relative",
-                  }}
-                >
-                  <Typography
-                    component="span"
-                    color="#9e9e9e"
-                    fontSize="12px"
-                    paddingBottom="8px"
-                    display="block"
-                  >
-                    編集日時：{jstFormattedDate(todo.time)}
-                  </Typography>
-                  <TextField
-                    id="modal-modal-text"
-                    variant="outlined"
-                    type="text"
-                    fullWidth
-                    value={input.text}
-                    error={input.text ? undefined : error}
-                    helperText={
-                      !input.text && error ? "内容を入力してください" : null
-                    }
-                    multiline
-                    rows={9}
-                    onChange={(e) => setInput({ ...input, text: e.target.value })}
-                  />
-                  <StatusPullList
-                    // statusプルダウン
-                    pullDownList={statusesPull}
-                    input={{ ...input, status: input.status }} // input.statusを渡す
-                    error={error}
-                    setInput={(statusInput) =>
-                      setInput({ ...input, status: statusInput.status })
-                    }
-                  />
-                  <CloseIcon
-                    // 閉じる
-  
-                    sx={{
-                      position: "absolute",
-                      top: "-27px",
-                      right: 0,
-                      color: "#FFF",
-                      cursor: "pointer",
-                    }}
-                    onClick={handleClose}
-                  />
-                  <Box
-                    sx={{
-                      width: "100%",
-                      display: "flex",
-                      justifyContent: "center",
-                      marginTop: 3,
-                    }}
-                  >
-                    <Button
-                      variant="contained"
-                      sx={{ display: "block" }}
-                      onClick={() => {
-                        saveTodo();
-                        if (input.text && input.status) {
-                          setModalIsOpen(false);
-                        }
-                      }}
-                    >
-                      保存
-                    </Button>
-                  </Box>
-                </Box>
-              </Box>
-            </Modal>
-          )}
-        />
-			</Button>
-    </>
+              保存
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Modal>
   );
 };
 
